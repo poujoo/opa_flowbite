4af4ab5316cf7aad74200f78417e6b08af3ce710
Elio <ubuntu@elionode1.lan>
2024-06-20 08:32:03 +0000

    Vanilla flowbite-react

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js"
   ],
   safelist:[
     {
@@ -20,6 +21,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 };
 export default config;
